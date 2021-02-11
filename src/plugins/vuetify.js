0812e4f80005250434b421ce95c5f2ec7d7cf56d
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-02-11 17:13:56 -0300

    customizando temas

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,19 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: colors.grey.darken3, // #E53935
+        secondary: colors.yellow.base, // #FFCDD2
+        accent: colors.shades.black,
+        error: colors.red.accent3
+      }
+    }
+  }
+});
