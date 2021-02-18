e878e9505a54bfa8df65a0bb997fa60bf59ebdc9
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-02-18 11:56:41 -0300

    add data to test, removing soon

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -51,13 +51,13 @@
       class="d-flex"
     >
       <v-col
-        class="d-flex align-start justify-space-around "
+        class="d-flex align-center py-2 justify-space-around"
         cols="4"
         align-self="start"
-        v-for="(partida, index) in matches"
-        :key="partida"
+        v-for="(match, index) in matches"
+        :key="match.gameId"
       >
-        <v-btn :href="partida" target="_blank" class="mt-6 pa-5"
+        <v-btn :href="match.link" target="_blank" class="mt-6 pa-5"
           >PARTIDA {{ index + 1 }}
         </v-btn>
       </v-col>
@@ -80,7 +80,284 @@ export default {
     nick2: "",
     json: "",
     loading: false,
-    matches: []
+    matches: [
+      {
+        platformId: "BR1",
+        gameId: 2185713116,
+        champion: 238,
+        queue: 900,
+        season: 13,
+        timestamp: 1613091032338,
+        role: "NONE",
+        lane: "JUNGLE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2185713116?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2184870184,
+        champion: 4,
+        queue: 900,
+        season: 13,
+        timestamp: 1613005879848,
+        role: "DUO",
+        lane: "BOTTOM",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2184870184?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2184856756,
+        champion: 64,
+        queue: 900,
+        season: 13,
+        timestamp: 1613004971344,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2184856756?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2184793704,
+        champion: 117,
+        queue: 900,
+        season: 13,
+        timestamp: 1613002920150,
+        role: "DUO",
+        lane: "TOP",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2184793704?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2184810151,
+        champion: 81,
+        queue: 900,
+        season: 13,
+        timestamp: 1613001757930,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2184810151?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2184776256,
+        champion: 67,
+        queue: 900,
+        season: 13,
+        timestamp: 1613000253290,
+        role: "DUO",
+        lane: "BOTTOM",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2184776256?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2184773452,
+        champion: 412,
+        queue: 900,
+        season: 13,
+        timestamp: 1612998989018,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2184773452?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183956868,
+        champion: 90,
+        queue: 900,
+        season: 13,
+        timestamp: 1612919301805,
+        role: "SOLO",
+        lane: "BOTTOM",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183956868?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183972514,
+        champion: 11,
+        queue: 900,
+        season: 13,
+        timestamp: 1612917720378,
+        role: "NONE",
+        lane: "JUNGLE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183972514?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183898159,
+        champion: 27,
+        queue: 900,
+        season: 13,
+        timestamp: 1612915914533,
+        role: "DUO_CARRY",
+        lane: "BOTTOM",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183898159?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183837265,
+        champion: 236,
+        queue: 900,
+        season: 13,
+        timestamp: 1612911374043,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183837265?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183825167,
+        champion: 20,
+        queue: 900,
+        season: 13,
+        timestamp: 1612910279900,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183825167?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183811712,
+        champion: 236,
+        queue: 900,
+        season: 13,
+        timestamp: 1612908663999,
+        role: "SOLO",
+        lane: "BOTTOM",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183811712?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183290529,
+        champion: 27,
+        queue: 900,
+        season: 13,
+        timestamp: 1612844053530,
+        role: "SOLO",
+        lane: "BOTTOM",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183290529?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183219277,
+        champion: 11,
+        queue: 900,
+        season: 13,
+        timestamp: 1612842602993,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183219277?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183207068,
+        champion: 133,
+        queue: 900,
+        season: 13,
+        timestamp: 1612840985886,
+        role: "DUO_SUPPORT",
+        lane: "BOTTOM",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183207068?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2183185010,
+        champion: 11,
+        queue: 900,
+        season: 13,
+        timestamp: 1612839504817,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2183185010?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2182385979,
+        champion: 107,
+        queue: 900,
+        season: 13,
+        timestamp: 1612759355550,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2182385979?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2182384426,
+        champion: 104,
+        queue: 900,
+        season: 13,
+        timestamp: 1612757812753,
+        role: "NONE",
+        lane: "JUNGLE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2182384426?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2182392658,
+        champion: 86,
+        queue: 900,
+        season: 13,
+        timestamp: 1612756564305,
+        role: "DUO_SUPPORT",
+        lane: "NONE",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2182392658?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2182400340,
+        champion: 202,
+        queue: 900,
+        season: 13,
+        timestamp: 1612754887008,
+        role: "DUO",
+        lane: "BOTTOM",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2182400340?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2182288215,
+        champion: 55,
+        queue: 900,
+        season: 13,
+        timestamp: 1612753248646,
+        role: "DUO_SUPPORT",
+        lane: "TOP",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2182288215?tab=overview"
+      },
+      {
+        platformId: "BR1",
+        gameId: 2182256417,
+        champion: 84,
+        queue: 900,
+        season: 13,
+        timestamp: 1612751628195,
+        role: "DUO",
+        lane: "TOP",
+        link:
+          "https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/2182256417?tab=overview"
+      }
+    ]
   }),
 
   methods: {
