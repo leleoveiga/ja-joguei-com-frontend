ddd0c9d078e6bddf001e16beda123fd252c3d199
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-09-22 21:35:13 -0300

    removing slider and add headline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,20 @@
 /* eslint-disable no-console */
 <template>
   <v-container fluid>
+    <div class="d-flex flex-column mt-16 align-center">
+      <span class="headline font-weight-bold"
+        >Saiba se você já jogou com alguém!</span
+      >
+      <v-subheader>
+        <span style="text-align: center">
+          nas últimas 50 partidas
+        </span>
+      </v-subheader>
+    </div>
     <v-text-field
       background-color=""
       class="mx-auto"
-      style="max-width: 300px; margin-top: 100px"
+      style="max-width: 300px; margin-top: 80px"
       placeholder="Seu nick"
       autocomplete="nick"
       v-model="inputNick1"
@@ -35,30 +45,6 @@
         </v-icon>
       </template>
     </v-text-field>
-    <v-subheader class="mx-auto" style="max-width: 335px;">
-      <span style="text-align: center">
-        Você pode escolher onde começa e termina o scan das partidas. Max.: 50
-        partidas
-      </span>
-    </v-subheader>
-    <v-range-slider
-      color="deep-purple darken-3"
-      thumb-color="secondary"
-      track-color="#202020"
-      max="100"
-      min="0"
-      class="mx-auto my-10"
-      style="max-width: 400px;"
-      v-model="minMax"
-      thumb-label="always"
-    ></v-range-slider>
-
-    <!-- <span
-      style="font-size: 10px; width: 200px; display: block;text-align: center"
-      class="mx-auto mt-n14 mb-10"
-    >
-      ( &lt; 0,5 segundo pra cada partida analisada )</span
-    > -->
     <v-btn
       class="mx-auto mb-12"
       style="width:120px; display: block;"
@@ -76,7 +62,6 @@
         transition="scroll-x-transition"
         :type="alertType"
       >
-        <!-- {{ alertMessage }} -->
         {{ errorMsg }}
       </v-alert>
     </div>
@@ -105,7 +90,7 @@ export default {
     errorMsg: "",
     alert: false,
     alertType: "info",
-    minMax: [0, 30],
+    minMax: [0, 50],
     matches: []
   }),
 
@@ -117,48 +102,28 @@ export default {
       return true;
     },
     async getMatches() {
-      if (this.isValidRange()) {
-        this.loading = true;
-        const count = this.minMax[1] - this.minMax[0];
-        const urlTarget = `/api/getMatches/${this.inputNick1}/${this.inputNick2}/${this.minMax[0]}/${count}`;
+      this.loading = true;
+      const count = this.minMax[1] - this.minMax[0];
+      const urlTarget = `/api/getMatches/${this.inputNick1}/${this.inputNick2}/${this.minMax[0]}/${count}`;
 
-        axios
-          .get(urlTarget)
-          .then(response => {
-            const { data } = response;
-            // [this.inputNick1, this.inputNick2] = ["", ""]; // limpar os nicks do input
-            [this.icon1, this.icon2] = [data.icon1, data.icon2];
-            this.matches = data;
-            this.loading = false;
-          })
-          .catch(error => {
-            this.errorMsg = `${error.response.status} ${error.response.statusText}`;
-            this.alert = true;
-            this.alertType = "error";
+      axios
+        .get(urlTarget)
+        .then(response => {
+          const { data } = response;
+          [this.icon1, this.icon2] = [data.icon1, data.icon2];
+          this.matches = data;
+          this.loading = false;
+        })
+        .catch(error => {
+          this.errorMsg = `${error.response.status} ${error.response.statusText}`;
+          this.alert = true;
+          this.alertType = "error";
 
-            setTimeout(() => {
-              this.alert = false;
-            }, 5000);
-            this.loading = false;
-          });
-      } else {
-        this.alert = true;
-        this.alertType = "warning";
-        setTimeout(() => {
-          this.alert = false;
-        }, 5000);
-      }
-    }
-  },
-  computed: {
-    alertMessage() {
-      if (this.alertType === "warning") {
-        return "Coloque um range menor ou igual à 50 !";
-      }
-      if (this.alertType === "error") {
-        return "Algo deu errado :( Atualize e tente novamente!";
-      }
-      return "Um erro desconhecido!";
+          setTimeout(() => {
+            this.alert = false;
+          }, 5000);
+          this.loading = false;
+        });
     }
   }
 };
